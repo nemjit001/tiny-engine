7f3c099f9d68ded80583797ba7f14f25deb3115e
Tijmen Verhoef <tijmenmenno@gmail.com>
2024-10-11 15:40:04 +0200

    added simple getters for cli args

--- src/core/commandline_args.cpp
+++ src/core/commandline_args.cpp
@@ -4,8 +4,46 @@ namespace tiny_engine
 {
 	CommandlineArgs::CommandlineArgs(int argc, char const** argv)
 	{
-		(void)(argc);
-		(void)(argv);
-		// TODO(nemjit001): Load CLI args as key-value pairs
+		this->argv.resize(argc);
+		for (int i = 0; i < argc; i++)
+		{
+			char const* pArg = argv[i];
+			this->argv[i] = pArg;
+		}
+	}
+
+	bool CommandlineArgs::isSet(std::string const& name) const
+	{
+		for (auto const& arg : argv)
+		{
+			if (arg == name) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	std::string CommandlineArgs::argValue(std::string const& name) const
+	{
+		bool found = false;
+		size_t argIdx = 0;
+		for (auto const& arg : argv)
+		{
+			if (arg == name)
+			{
+				found = true;
+				break;
+			}
+
+			argIdx++;
+		}
+
+		// Check if next value is within argv bounds, valiation of retrieved value is left to caller
+		if (found && (argIdx + 1) < argv.size()) {
+			return argv[argIdx + 1];
+		}
+
+		return {};
 	}
 } // namespace tiny_engine
--- src/core/commandline_args.hpp
+++ src/core/commandline_args.hpp
@@ -1,11 +1,27 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace tiny_engine
 {
-	/// @brief Commandline argument store, parsed from `argv` passed to main.
+	/// @brief Commandline argument store, parsed from the `argv` passed to main.
 	class CommandlineArgs
 	{
 	public:
 		CommandlineArgs(int argc, char const** argv);
+
+		/// @brief Check if a commandline variable is set.
+		/// @param name Entire name of the variable, with leading symbols (i.e. "foo" is different from "--foo").
+		/// @return A boolean indicating if the variable is set.
+		bool isSet(std::string const& name) const;
+
+		/// @brief Retrieve the value associated with a commandline argument.
+		/// @param name Entire name of the variable, with leading symbols.
+		/// @return An empty string if not set, or value is missing.
+		std::string argValue(std::string const& name) const;
+
+	public:
+		std::vector<std::string> argv;
 	};
 } // namespace tiny_engine
