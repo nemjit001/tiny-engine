732e70aa67b7cb3b5e18ed4232eb2ea5dc900d0b
Tijmen Verhoef <tijmenmenno@gmail.com>
2024-10-13 13:11:45 +0200

    added Volk dependency

--- vendored/vendored.cmake
+++ vendored/vendored.cmake
@@ -10,8 +10,11 @@ set(GLFW_BUILD_EXAMPLES OFF)
 set(GLFW_BUILD_TESTS OFF)
 add_subdirectory("${VENDORED_LIB_BASE_DIR}/glfw/")
 
+# Add Volk subproject
+add_subdirectory("${VENDORED_LIB_BASE_DIR}/volk/")
+
 if (${TINY_ENGINE_BUILD_TESTS})
 	# Add GoogleTest subproject
 	set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-	add_subdirectory("${VENDORED_LIB_BASE_DIR}/googletest")
+	add_subdirectory("${VENDORED_LIB_BASE_DIR}/googletest/")
 endif()
