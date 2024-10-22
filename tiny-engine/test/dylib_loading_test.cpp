814712a5529cbe7b70d310b2948620d627a4330b
Tijmen Verhoef <tijmenmenno@gmail.com>
2024-10-22 10:17:03 +0200

    added test library for dylib loading tests

--- tiny-engine/test/dylib_loading_test.cpp
+++ tiny-engine/test/dylib_loading_test.cpp
@@ -14,9 +14,25 @@ namespace tiny_engine_core
 	TEST(DyLibLoadingTests, TestLibLoading)
 	{
 		// TODO(nemjit001): Load test library with known name & function pointers
-		tiny_engine::core::DyLibHandle* pHandle = tiny_engine::core::DyLib::open("TinyEngine-shared");
+		tiny_engine::core::DyLibHandle* pHandle = tiny_engine::core::DyLib::open("TestDyLib");
 		ASSERT_NE(pHandle, nullptr);
 		ASSERT_TRUE(tiny_engine::core::DyLib::loaded(pHandle));
+		tiny_engine::core::DyLib::close(pHandle);
+	}
+
+	TEST(DyLibLoadingTests, TestFuncLoading)
+	{
+		// TODO(nemjit001): Load test library with known name & function pointers
+		tiny_engine::core::DyLibHandle* pHandle = tiny_engine::core::DyLib::open("TestDyLib");
+		ASSERT_NE(pHandle, nullptr);
+		ASSERT_TRUE(tiny_engine::core::DyLib::loaded(pHandle));
+
+		tiny_engine::core::PFN_TinyEngineVoidFunc pFunc = tiny_engine::core::DyLib::getProcAddr(pHandle, "func");
+		ASSERT_NE(pHandle, nullptr);
+
+		int(TINY_ENGINE_APICALL *addFunc)(int, int) = reinterpret_cast<int(TINY_ENGINE_APICALL *)(int, int)>(pFunc);
+		ASSERT_EQ(addFunc(1, 1), 2);
+
 		tiny_engine::core::DyLib::close(pHandle);
 	}
 } // namespace tiny_engine_core
